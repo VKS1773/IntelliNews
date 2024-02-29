ea707b3ed4ce4ca5945657d730ee598ba47859d2
Vikash Sharma <vs8824394430@gmail.com>
2024-02-29 23:30:33 +0530

    added changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 import NewsCards from "./Components/NewsCards/NewsCards.js";
+import alanai from "./Components/image/alan-ai.png";
 
 const alanKey =
   "ecc705bfc0c27cb9f26b571aff795c712e956eca572e1d8b807a3e2338fdd0dc/stage";
@@ -18,7 +19,21 @@ const App = () => {
   }, []);
   return (
     <div>
-      <h1>Hello there is a world</h1>
+      <div
+        style={{
+          width: "40vw",
+          height: "40vh",
+          margin: "20px auto",
+          backgroundColor: "green",
+          borderRadius: 10,
+        }}
+      >
+        <img
+          style={{ width: "80%", height: "80%", margin: "auto" }}
+          src={alanai}
+          alt=" alan-ai logo"
+        />
+      </div>
       <div>
         <NewsCards articles={newsarticles} />
       </div>
